b73635e2cbb14d53f6d19701084e8ebf46c3b063
hyebine <berryme015@gmail.com>
2023-12-27 15:23:45 +0900

    component > Hd.js 완성 및 public css와 img 넣기

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+//bine 작업
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Hd from "./component/bine/Hd";
+
+
 
 function App() {
   return (
     <>
+      <Hd></Hd>
+
 
     </>
   );
--- src/component/bine/Hd.js
+++ src/component/bine/Hd.js
@@ -1,10 +1,27 @@
-import React from 'react'
-
 function Hd() {
     return (
-        <>
-        </>
-    )
-}
-
-export default Hd
+      <>
+  
+        <header id="hd">
+          <div className="minibanner text-center">
+            <a href="https://www.jeju.go.kr/booking/index.htm" target="_blank">통합 예약</a>
+          </div>
+          <div className="container max-w1280 d-flex justify-content-between align-items-center mx-auto">
+            <a href="#" className="logo"><img src="/img/logo.png" alt="로고이미지" /></a>
+            <ul id="gnbs" className="d-flex justify-content-around">
+              <li><a href="#Event" className="scrollmenu">행사</a></li>
+              <li><a href="#Tourism" className="scrollmenu">관광명소</a></li>
+              <li><a href="#video" className="scrollmenu">홍보영상</a></li>
+              <li><a href="#discountEvent" className="scrollmenu">이벤트</a></li>
+              <li><a href="#route" className="scrollmenu">투어경로추천</a></li>
+              <li><a href="#Comments" className="scrollmenu">커뮤니티</a></li>
+            </ul>
+          </div>
+        </header>
+      </>
+  
+    );
+  }
+  
+  export default Hd
+  
